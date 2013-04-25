ea09beb349434333b2c2b64f3171561fc0a49a0e
Matthias Geier <mayutamano@gmail.com>
2013-04-25 11:45:21 +0200

    Initial push

---
+++ lib/parser_girl.rb
@@ -0,0 +1,87 @@
+class ParserGirl
+  def initialize(xml=nil)
+    @xml = xml
+    @result = nil
+    @stack = nil
+  end
+
+  def find(needle, haystack_base=nil)
+    haystack_base = @xml unless haystack_base
+    return [] unless haystack_base
+
+    haystack = haystack_base
+    pos = 0
+    @result = []
+    @stack = []
+    while true do
+      if haystack =~ /\<([^!][^\>]*)\>/i
+        content = $1 # tag-hit
+        b = $`.length + 1 # relative beginning in haystack
+        e = content.length + b + 1 # relative ending in haystack
+        if content =~ /^([^\s]+)/ and $1.downcase == "script" and
+            haystack =~ /\<\/script(\s[^\>]*)?\>/i
+          e = $`.length + $&.length
+        end
+
+        if content =~ /^#{needle}(\s.*)?$/i
+          #puts "Found begin at #{b}, #{e}, #{pos}: #{content}"
+          push({:position => pos+e, :attrs => split_attr($1)})
+        elsif content =~ /^\/#{needle}(\s.*)?$/i
+          #puts "Found end at #{b}, #{e}, #{pos}: #{content}"
+          hash = pop(haystack_base, b+pos-1)
+          if hash
+            if block_given?
+              @result.push(yield(hash[:content], hash[:attrs]))
+            else
+              @result.push(hash[:content])
+            end
+          end
+        end
+        pos += e
+        haystack = haystack_base[pos, haystack_base.length-pos]
+      else
+        break
+      end
+    end
+    @result
+  end
+
+  private
+  def push(hash)
+    @stack.push hash
+  end
+
+  def pop(haystack, current_position)
+    if @stack.any?
+      hash = @stack.pop
+      hash[:content] = haystack[hash[:position],
+        current_position-hash[:position]]
+      hash[:end_position] = current_position
+      hash
+    end
+  end
+
+  def split_attr(attrs)
+    attr_hash = {}
+    while(1)
+      if attrs =~ /\s*([^=]+)=((\"([^\"]+)\")|(\'([^\']+)\')|([^\s]+))/
+        #puts "$1=#{$1} $2=#{$2} $3=#{$3} $4=#{$4} $5=#{$5} $6=#{$6} $7=#{$7} "
+        attrs = $'
+        key = $1
+        value = nil
+        value = $7 if $7
+        value = $6 if $6
+        value = $4 if $4
+        if value
+          value.gsub! "\"", "\\\""
+          value.gsub! "'", "\\'"
+        end
+        attr_hash[key.downcase.to_sym] = value
+      else
+        break
+      end
+    end
+    attr_hash
+  end
+end
+
---
+++ parser_girl.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.name = "ParserGirl"
+  s.version = '1.0.0'
+  s.summary = "A minimal very fast xml/html parser"
+  s.author = "Matthias Geier"
+  s.homepage = "https://github.com/matthias-geier/parser_girl"
+  s.require_path = 'lib'
+  s.files = Dir['lib/*.rb']
+  s.required_ruby_version = '>= 1.9.1'
+end
